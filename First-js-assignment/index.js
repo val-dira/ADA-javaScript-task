8d5fef375c16a39e63daee24b3b4a15275d0e1bc
val-dira <oliobival@gmail.com>
2022-12-21 14:32:05 -0800

    first js upload

---
+++ First-js-assignment/index.js
@@ -0,0 +1,12 @@
+// Declare Two Variables - Username and age
+
+let username;
+let age;
+
+// Ask user to enter their name, then their age, store this information in the appropriate variable then 
+username = "perculiar";
+age = 30;
+
+// Write an alert message with a sentence reading “Welcome, <insert the name variable>, You are <insert the age variable> years old.
+
+alert(`welcome, ${username}, you are ${age} years old`)
---
+++ javascript basic/index.js
@@ -0,0 +1,4 @@
+alert("Hello");// This is a statement
+alert("I am JavaScript"); //This is a statement
+// Statements can be seperated with semicolon.
+
